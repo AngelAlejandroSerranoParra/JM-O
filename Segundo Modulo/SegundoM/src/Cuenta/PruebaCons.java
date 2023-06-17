94ffd7456210a906537466dab4af74cbf5fb880b
Alex Alejandro <t.f.g.12@hotmail.com>
2023-06-17 11:49:43 -0600

    Modulo 2 Finalizado

---
+++ Segundo Modulo/SegundoM/src/Cuenta/PruebaCons.java
@@ -0,0 +1,17 @@
+package Cuenta;
+
+public class PruebaCons {
+    public static void main(String[] args) {
+         Cuenta cuenta = new Cuenta(555);
+         Cuenta cuenta2=new Cuenta(1215);
+         Cuenta cuenta3=new Cuenta(1212);
+         
+         System.out.println(Cuenta.getTotal());
+         System.out.println(cuenta2);
+                  System.out.println(cuenta);
+                           System.out.println(cuenta3);
+
+
+         
+    }
+}
